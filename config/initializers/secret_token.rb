f8d7143b06290237b1ca079cf360e02ea235a17f
Rana <tanim.daw@gmail.com>
2013-08-13 20:52:37 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Visionboard::Application.config.secret_token = '97a5780878b53ed0298c04efcc31ecde92a7b9a5aa41ef8d06f6982152e3f860b1446c0ac857a6400e88bec1ab8a9c10279b4815f8a06ee980164fd7e0fe1da3'
